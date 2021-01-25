8cfd3372ef040292ae54473dca6dc6cb49296fda
Kentwtf <lvlcs@mail.ru>
2021-01-25 18:33:10 +0300

    GameOfUsers
    Создать массив из слов (в массиве может быть сколько угодно элементов).
    
    При запуске программы, должно загадываться слово, спрашиваться ответ у пользователя, с последующим сравнением его с загаданным словом. После ввода слова должно сообщаться, правильно ли ответил пользователь. Если слово угадано неверно, компьютер показывает буквы, которые стоят на своих местах.

---
+++ New.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class New {
+
+    private static List<String> words = Arrays.asList("Корова", "Бык", "Лось", "Олень", "Лань",
+            "Волк", "Суррикат", "Медведь", "Заяц", "Голубь", "Лиса",
+            "Поросенок", "Суслик", "Сова", "Воробей", "Гусь", "Лебедь", "Полёвка",
+            "Выпь", "Ворон", "Лошадь", "Единорог", "Гном", "Леприкон",
+            "Эльф");
+
+    public static void main(String[] args) throws IOException {
+        String guess = getRandomWord(words);
+        String maskedGuess = maskWord(guess);
+        System.out.println("Что у меня в карманцах, моя прелессть?");
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            String myWord = br.readLine();
+            if (guess.equals(myWord)) {
+                System.out.println("Ты победил, " + guess + " действительно находится у меня в карманцах!");
+                break;
+            } else {
+                System.out.println(maskedGuess);
+                System.out.println("Неверно, попробуй еще раз :)");
+            }
+        }
+
+    }
+
+    private static String getRandomWord(List<String> words) {
+        Random r = new Random();
+        return words.get(r.nextInt(words.size() - 1));
+    }
+
+    private static String maskWord(String word) {
+        if (word == null || word.isEmpty()) return "";
+        int showCount = word.length() == 1? 1: 2;
+        String mask = "#############";
+        return word.substring(0, showCount) + mask;
+    }
+}
